6d78979035192a44211111c111be823aa021734b
“az” <shimazaki@aaa.email.ne.jp>
2021-06-11 15:33:54 +0900

    add button and daylist

---
+++ src/components/DayList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import DayListItem from './DayListItem'
+
+export default function DayList(props) {
+
+  return (
+    <ul>
+      {
+        props.days.map((day, i)=><DayListItem
+         key={i} 
+         spots={day.spots}
+         name={day.name}
+         setDay={props.setDay}
+         selected={day.name === props.day}
+        />)
+      }
+    </ul>
+  )
+}
