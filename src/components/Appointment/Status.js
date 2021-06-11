ec46e262ce19d76264728d9dbc127917abc05338
“az” <shimazaki@aaa.email.ne.jp>
2021-06-11 19:44:28 +0900

    update sub components

--- src/components/Appointment/Status.js
+++ src/components/Appointment/Status.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function Status() {
+export default function Status(props) {
   return (
-    <div>
-      
-    </div>
+    <main className="appointment__card appointment__card--status">
+      <img
+        className="appointment__status-image"
+        src="images/status.png"
+        alt="Loading"
+      />
+      <h1 className="text--semi-bold">{props.message}</h1>
+    </main>
   )
 }
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Header from './Header'
+import './styles.scss'
 
 export default function Appointment(props) {
   return (
